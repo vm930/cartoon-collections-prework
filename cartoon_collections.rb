51c9317fa17184c976e7293d14d2f8e6e83ffc66
abstract-animation-9914 <vm930@hotmail.com>
2019-01-11 19:23:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # code an argument here
   # Your code here
-  planeteer_calls.map { |n| puts"#{n}!" }
+  planeteer_calls.map { |n| n.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
