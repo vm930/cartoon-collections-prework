11cf2e7ea0260ff680d9ce051a451932ee95c5a5
abstract-animation-9914 <vm930@hotmail.com>
2019-01-11 19:38:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarves)
+	dwarves.each_with_index { |name,i| puts "#{i+1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
+  # code an argument here
   # Your code here
+  planeteer_calls.map { |n| n.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(phrase)# code an argument here
   # Your code here
+  phrase.all? {|n| n.length >= 4?}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese# code an argument ere
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
