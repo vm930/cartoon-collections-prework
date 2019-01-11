399efa7ed5f91944acca6aa06c2f33bcaa68a2ad
abstract-animation-9914 <vm930@hotmail.com>
2019-01-11 19:30:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(phrase)# code an argument here
   phrase.collect {|n| n.length > 5}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese# code an argument ere
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
