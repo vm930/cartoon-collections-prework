ff95e2e6bc1ad729cd3a8753ede47e51af715761
abstract-animation-9914 <vm930@hotmail.com>
2019-01-11 19:30:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def long_planeteer_calls(phrase)# code an argument here
   # Your code here
-  phrase.new{|n| n.length > 5}
+  phrase.collect {|n| n.length > 5}
 end
 
 def find_the_cheese# code an argument here
